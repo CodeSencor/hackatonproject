e9429378d8dc5ff766772e164453a14a546693d1
kittenfromvoid <etooilikespizza@gmail.com>
2021-10-04 11:07:29 +0200

    init commit, init spring backend

---
+++ backend/src/main/kotlin/pl/codesencor/foodexchangebackend/FoodexchangebackendApplication.kt
@@ -0,0 +1,11 @@
+package pl.codesencor.foodexchangebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodexchangebackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoodexchangebackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/pl/codesencor/foodexchangebackend/FoodexchangebackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.codesencor.foodexchangebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoodexchangebackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
